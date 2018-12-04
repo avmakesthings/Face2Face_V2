51790829fc495cf6c57b8b01a3ae889d89da676e
AV <av@anastasia.io>
2018-12-03 22:13:07 -0800

    face focus square in progress

--- Assets/_Scripts/FaceTracking/FaceVisualizer.cs
+++ Assets/_Scripts/FaceTracking/FaceVisualizer.cs
@@ -14,12 +14,16 @@ public class FaceVisualizer : MonoBehaviour {
 	private Mesh faceDebugMesh;
 	private UnityARSessionNativeInterface m_session;
 
-	Bounds bounds;
-	Vector3 basePos;
-	GameObject bBox;
+    private Bounds meshbounds;
+    private Bounds rendBounds;
+	private Vector3 basePos;
+    private Vector2 screenBasePos;
+	private GameObject bBox;
+    private bool FaceActive = false;
 
 	public Material faceBoxMaterial;
-
+    public Texture2D crosshair;
+    public Rect position;
 
 	// Use this for initialization
 	void Start () {
@@ -32,8 +36,6 @@ public class FaceVisualizer : MonoBehaviour {
 		config.alignment = UnityARAlignment.UnityARAlignmentGravity;
 		config.enableLightEstimation = true;
 
-        //Debug.Log("arkit face session configured");
-
 		if (config.IsSupported) {
 			
 			m_session.RunWithConfig (config);
@@ -43,16 +45,30 @@ public class FaceVisualizer : MonoBehaviour {
 			UnityARSessionNativeInterface.ARFaceAnchorRemovedEvent += FaceRemoved;
 
 		}
-		
-
 	}
+
+
+    void Update()
+    {
+        
+        //if(FaceActive){
+        //    screenBasePos = Camera.main.WorldToScreenPoint(basePos);
+        //    position = new Rect(screenBasePos.x, screenBasePos.y, 400,400); 
+        //}
+        //else{
+        //    position = new Rect((Screen.width - crosshair.width) / 2, (Screen.height - crosshair.height) / 2, crosshair.width, crosshair.height); 
+        //}
+    }
+
+
 
 	void FaceAdded (ARFaceAnchor anchorData)
 	{
 		gameObject.transform.localPosition = UnityARMatrixOps.GetPosition (anchorData.transform);
 		gameObject.transform.localRotation = UnityARMatrixOps.GetRotation (anchorData.transform);
-        //Debug.Log("face added");
 	}
+
+
 
 	void FaceUpdated (ARFaceAnchor anchorData)
 	{
@@ -60,35 +76,42 @@ public class FaceVisualizer : MonoBehaviour {
 		gameObject.transform.localPosition = UnityARMatrixOps.GetPosition (anchorData.transform);
 		gameObject.transform.localRotation = UnityARMatrixOps.GetRotation (anchorData.transform);
 
-		//draw face mesh
 		updateDebugFaceMesh(anchorData);
-        //Debug.Log("face updated");
+
+        if(!FaceActive){
+            FaceActive = true;
+        }
 	}
+
+
 
 
 	void FaceRemoved (ARFaceAnchor anchorData)
 	{
-		meshFilter.mesh = null;
+        FaceActive = false;
+        meshFilter.mesh = null;
 		faceDebugMesh = null;
-        //Debug.Log("face removed");
+
 	}	
+
 
 
 	void createDebugFaceMesh(ARFaceAnchor anchorData){
 		
-        //Debug.Log("create debug face mesh");
         faceDebugMesh = new Mesh ();
 		meshFilter.mesh = faceDebugMesh;
-
-		//drawBBox();
+		//drawBBox();  // this is for the 3d bounding box
 		updateDebugFaceMesh(anchorData);
+        //basePos = anchorData.transform;
+
+        meshbounds = faceDebugMesh.bounds;
+        rendBounds = this.GetComponent<MeshRenderer>().bounds;
 
 	}
 
 
-	void updateDebugFaceMesh(ARFaceAnchor anchorData){
 
-        //Debug.Log("update debug face mesh");
+	void updateDebugFaceMesh(ARFaceAnchor anchorData){
 
 		if(faceDebugMesh == null){
 			createDebugFaceMesh(anchorData);
@@ -98,23 +121,27 @@ public class FaceVisualizer : MonoBehaviour {
 		faceDebugMesh.triangles = anchorData.faceGeometry.triangleIndices;
 		faceDebugMesh.RecalculateBounds();
 		faceDebugMesh.RecalculateNormals();
-			
-		// updateBBox(faceDebugMesh);
-		// bBox.transform.position = bounds.center;
-
 
 	}
 
-	void drawBBox(){
-		bounds = this.GetComponent<MeshRenderer>().bounds;
-		basePos = bounds.center;
-		bBox = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		bBox.transform.parent = transform;
-		bBox.transform.position = basePos;
-		bBox.transform.localScale = new Vector3(0.3f, 0.25f, 0.11f);
-		// bBox.transform.rotation = new Quaternion(-10f,10f,-86f, 0f);
-		// bBox.transform.position = new Vector3(0,0.02f,0);
-		bBox.GetComponent<Renderer>().material = faceBoxMaterial;
-	}
+
+
+	//void drawBBox(){
+	//	bounds = this.GetComponent<MeshRenderer>().bounds;
+	//	basePos = bounds.center;
+	//	bBox = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	//	bBox.transform.parent = transform;
+	//	bBox.transform.position = basePos;
+	//	bBox.transform.localScale = new Vector3(0.3f, 0.25f, 0.11f);
+	//	bBox.GetComponent<Renderer>().material = faceBoxMaterial;
+	//}
+
+
+
+    void OnGUI()
+    {
+        //GUI.DrawTexture(position, crosshair);
+    }
+
 
 }
